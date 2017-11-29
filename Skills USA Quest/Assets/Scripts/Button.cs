e1b195654188d2c7309216cf73b271a7a832d72d
MrHerty <umbays@yahoo.com>
2017-11-29 07:47:25 -0600

    physics figured out
    moving stuff with your face, buttons still do not work

---
+++ Skills USA Quest/Assets/Scripts/Button.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+
+	public AudioSource audioSource;
+
+
+	void onCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.tag == "Player")
+		{
+			Debug.Log ("Button Pressed");
+			audioSource.Play ();
+		}
+	}
+}
